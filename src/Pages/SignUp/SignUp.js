eca42e09d415e0712a456f0cf55e8b0560c370e6
Sizan75 <sakiuzzaman26@gmail.com>
2023-09-07 14:24:22 +0600

    Signup done

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,123 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import useToken from '../../hooks/useToken';
+import { AuthContext } from '../../context/AuthProvider';
+
+const SignUp = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [signUpError, setSignUPError] = useState('')
+    const { createUser, updateUser } = useContext(AuthContext);
+    const imageHostKey = process.env.REACT_APP_img_key;
+    const [createdUserEmail, setCreatedUserEmail]= useState('')
+    const [token]= useToken(createdUserEmail)
+    const navigate = useNavigate()
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/'
+
+        navigate('/')
+    
+    const handleSignUp = (data) => {
+        const image = data.photo[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+        const url = `https://api.imgbb.com/1/upload?&key=${imageHostKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })           
+         .then(res => res.json())
+        .then(imgdata => {
+            const users = {
+                image: imgdata.data.url,
+                userName: data.userName,
+                email: data.email,
+                bio: data.bio
+            }
+
+            localStorage.setItem(JSON.stringify(users))
+        })
+
+        setSignUPError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                toast('User Created Successfully.')
+                
+                const userInfo = {
+                    userName: data.name,
+                    profilePicture: data.profilePicture,
+                    email: data.email,
+                    bio: data.bio
+                }
+                updateUser(userInfo)
+                    .then(() => {
+                        localStorage.setItem('users', JSON.stringify(userInfo))             
+                     
+                    })
+                    .catch(err => console.log(err));
+            })
+            .catch(error => {
+                console.log(error)
+                setSignUPError(error.message)
+            });
+    }
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">User Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is Required"
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Image</span></label>
+                        <input type="file" {...register("photo", {
+                            required: "Photo is Required"
+                        })}className= "block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none" />
+                        {errors.image && <p className='text-red-500'>{errors.image.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email" {...register("email", {
+                            required: true
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "Password must be 6 characters long" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Bio</span></label>
+                        <input type="textarea" {...register("bio", {
+                            
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.bio && <p className='text-red-500'>{errors.bio.message}</p>}
+                    </div>
+
+                    <input className='btn btn-success w-full mt-4' value="Sign Up" type="submit" />
+                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                </form>
+                <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
+                <div className="divider">OR</div>
+               
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
